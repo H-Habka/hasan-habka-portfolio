8c80abf8311560bb294158404200da3d5f04bf6a
Wael <wael@users.noreply.github.com>
2026-09-08 17:29:06 +0000

    Deploy naval WOW homepage 3D hull studio (a92bad7)

--- static/js/425.cd54657b.chunk.js
+++
@@ -1,2 +0,0 @@
-"use strict";(self.webpackChunkportfolio=self.webpackChunkportfolio||[]).push([[425],{7425:function(t,a,h){h.d(a,{b9S:function(){return r}});var n=h(9983);function r(t){return(0,n.w_)({tag:"svg",attr:{viewBox:"0 0 24 24"},child:[{tag:"g",attr:{},child:[{tag:"path",attr:{fill:"none",d:"M0 0h24v24H0z"}},{tag:"path",attr:{d:"M10 3v2H5v14h14v-5h2v6a1 1 0 0 1-1 1H4a1 1 0 0 1-1-1V4a1 1 0 0 1 1-1h6zm7.707 4.707L12 13.414 10.586 12l5.707-5.707L13 3h8v8l-3.293-3.293z"}}]}]})(t)}}}]);
-//# sourceMappingURL=425.cd54657b.chunk.js.map
